b2796560d7a451066ab4d58be44b6708d8e42f43
MathisPhg <mathis.phg@gmail.com>
2026-04-04 03:47:49 +0200

    feat(404): creation de la page 404

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
 
 
+spl_autoload_register(function($classname){
+    $path = lcfirst(str_replace('\\','/',$classname));
+    $filename = $path.'.php';
+
+    if(file_exists($filename)){
+        include $filename;
+    }
+});
 
 
 
 
-$content = "views/project/project.phtml";
+
+
+
+
+$content = "views/404/404.phtml";
 $title = "test";
 
 include ("views/layout.phtml");
