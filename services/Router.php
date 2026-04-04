5e0c818c55855b4f2f7f38328fd506130314b831
MathisPhg <mathis.phg@gmail.com>
2026-04-04 19:41:26 +0200

    feat(Router): redirection automatique vers la page 404

--- services/Router.php
+++ services/Router.php
@@ -12,12 +12,23 @@ class Router
     public function __construct(string $page = "home")
     {
 
+        try {
+            if (!array_key_exists($page, AVAILABLE_ROUTES)) {
+                throw new \Exception("Page not found");
+            }
+        } catch (\Exception $e) {
+
+            $page = "404";
+        }
         $this->controller = AVAILABLE_ROUTES[$page]['controller'];
         $this->method = AVAILABLE_ROUTES[$page]['method'];
+
+
     }
 
     public function getController(): void
     {
+        
         $instance = "Controllers\\" . $this->controller;
 
         $controller = new $instance();
